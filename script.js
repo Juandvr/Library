9511ebe33f680f4f6b0c9136650f4f3f58aa41bc
Juandvr <villabonajuan203@gmail.com>
2024-07-07 14:50:15 -0500

    Add function to display  the books that are in the array

--- script.js
+++ script.js
@@ -1,9 +1,49 @@
 const myLibrary = [];
 
-function Book() {
-  
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+
+  this.info = () => `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
 }
+
+const theHobbit = new Book('The Hobbit', 'Tolkien', 95, 'not read');
+const mobyDick = new Book('Moby Dick', 'Herman Melville', 635, 'not read');
+const mockingbird = new Book('To kill a mockingbird', 'Harper Lee', 384, 'not read');
+
+myLibrary.push(theHobbit);
+myLibrary.push(mobyDick);
+myLibrary.push(mockingbird);
 
 function addBookToLibrary() {
   // do stuff here
 }
+
+function displayLibrary() {
+  const library = document.querySelector('.library');
+
+  myLibrary.forEach((book) => {
+    let div = document.createElement('div');
+    div.setAttribute('class', 'book');
+    library.appendChild(div);
+
+    let title = document.createElement('p');
+    let author = document.createElement('p');
+    let pages = document.createElement('p');
+    let read = document.createElement('p');
+
+    title.innerHTML = `Title: ${book.title}`;
+    author.innerHTML = `Author: ${book.author}`;
+    pages.innerHTML = `Pages: ${book.pages}`;
+    read.innerHTML = `Read status: ${book.read}`;
+
+    div.appendChild(title);
+    div.appendChild(author);
+    div.appendChild(pages);
+    div.appendChild(read);
+  })
+}
+
+window.onload = displayLibrary();
